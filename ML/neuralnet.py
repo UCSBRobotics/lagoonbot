aa59b00f78494987ff530274e431c33ce635bc66
JonathanCheng123 <jcmohr9@gmail.com>
2021-11-20 10:56:10 -0800

    scuffed code for matthew to see

--- ML/neuralnet.py
+++ ML/neuralnet.py
@@ -3,15 +3,24 @@ from random import seed
 import numpy as np
 
 def init_net(n_inputs, n_hidden, n_output):
-    network = []
-    hidden = [{"weights" : [random() for j in range(n_inputs+1)]} for i in range(n_hidden)]
-    network.append(hidden)
-    output = [{"weights" : [random() for j in range(n_hidden+1)]} for i in range(n_output)]
-    network.append(output)
-    return network
+    #network = []
+    #hidden = array([{"weights" : [random() for j in range(n_inputs+1)]} for i in range(n_hidden)])
+    #network.append(network, hidden)
+    #output = array([{"weights" : [random() for j in range(n_hidden+1)]} for i in range(n_output)])
+    #network.append(network, hidden)
+    #return network
+    hidden = np.array([{"weights" : [np.random.rand(n_inputs+1)]} for i in range(n_hidden)])
+    output = np.array([{"weights" : [np.random.rand(n_hidden+1)]} for i in range(n_output)])
+    print(hidden.dtype)
+    #network = []
+    #network.append(hidden)
+    #network.append(output)
+    return np.append([hidden], [output])
+    #return np.array(network)
 
 #test = init_net(2, 3, 2)
 #print (test)
+
 def activation(input_x):
     return 1 / (1 + (np.exp(-input_x)))
 
@@ -30,6 +39,7 @@ def forward_prop(network, row):
     for layer in network:
         new_inputs = []
         for neuron in layer:
+            print(type(neuron))
             activation1 = lin(neuron['weights'], inputs)
             neuron['output'] = activation(activation1)
             new_inputs.append(neuron['output'])
@@ -37,6 +47,6 @@ def forward_prop(network, row):
     return inputs
 
 test = init_net(3, 2, 2)
-row = [1, 0, None]
+row = [1, 0, 1]
 output = forward_prop(test, row)
 print (output)
